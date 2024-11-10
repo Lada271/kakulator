8ab0ffc7d84eac4f5cf7c4582cc47a095422f36d
Lada <ladaseredaa@gmail.com>
2024-11-10 12:36:21 +0500

    Fixed Zero Division Bug

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ b = float(input())
 sum = a + b
 dif = a - b
 com = a * b
-pri = a / b
+
 print(str(a) + " + " + str(b) + " = " + str(sum))
 print(str(a) + " - " + str(b) + " = " + str(dif))
 print(str(a) + " * " + str(b) + " = " + str(com))
-print(str(a) + " / " + str(b) + " = " + str(pri))
+if b != 0:
+    pri = a / b
+    print(str(a) + " / " + str(b) + " = " + str(pri))
 
