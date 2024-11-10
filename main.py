a0ff7bfd00b1f1ff6cdc17cbd4137a16ebb435b7
Lada <ladaseredaa@gmail.com>
2024-11-10 12:20:21 +0500

    Difference, composition, private

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ print ('Введите второе число')
 b = float(input())
 
 sum = a + b
+dif = a - b
+com = a * b
+pri = a / b
 print(str(a) + " + " + str(b) + " = " + str(sum))
+print(str(a) + " - " + str(b) + " = " + str(dif))
+print(str(a) + " * " + str(b) + " = " + str(com))
+print(str(a) + " / " + str(b) + " = " + str(pri))
+
